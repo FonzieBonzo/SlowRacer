220cc98387c0d3847478f0df7aef17cca911cad5
CrewBene <124531000+CrewBene@users.noreply.github.com>
2023-03-09 21:20:33 +0100

    First version

--- README.md
+++
@@ -1,2 +0,0 @@
-# SlowRacer
-SlowRacer
---
+++ SlowRacer/Index.razor.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Json;
+using System.Threading.Tasks;
+using SlowRacer.Components;
+
+namespace SlowRacer
+{
+    public partial class Index
+    {
+        int _index = 0;
+        int[][] _data;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _data = await Http.GetFromJsonAsync<int[][]>("Data/Track1.json");
+            Update();
+        }
+
+        async void Update()
+        {
+            while (true)
+            {
+                try
+                {
+                    await Task.Delay(100);
+                    MoveCar();
+                }
+                catch { }
+            }
+        }
+
+        void MoveCar()
+        {
+            _index += 10;
+            if (_index >= _data.Length)
+            {
+                _index = 0;
+            }
+
+            Car.Cars[0].Left = _data[_index][0] - 10;
+            Car.Cars[0].Top = _data[_index][1] - 10;
+            Car.Cars[0].Update();
+        }
+    }
+}
